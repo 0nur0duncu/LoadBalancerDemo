69f1bc8b4ba19da1087a631759fb0c3fe5f7db4a
Onur Oduncu <160393966+0nur0duncu@users.noreply.github.com>
2025-01-21 14:42:27 +0300

    Update main.go

--- golangService/main.go
+++ golangService/main.go
@@ -7,13 +7,23 @@ import (
 	"os"
 )
 
-func Handler(w http.ResponseWriter, r *http.Request) {
+func getResultHandler(w http.ResponseWriter, r *http.Request) {
 	ServerID := os.Getenv("SERVERID")
 	io.WriteString(w, "msg from server"+ServerID)
 }
 
+func rootHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
+	ServerID := os.Getenv("SERVERID")
+	io.WriteString(w, "Welcome to server "+ServerID)
+}
+
 func main() {
-	http.HandleFunc("/getResult", Handler)
+	http.HandleFunc("/", rootHandler)
+	http.HandleFunc("/getResult", getResultHandler)
 
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatal(err)
